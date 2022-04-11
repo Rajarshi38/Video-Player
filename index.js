e274a5049042c9e1090d9bbad376110972573ab0
Rajarshi38 <rajarshibiswas38@gmail.com>
2022-04-11 19:32:45 +0530

    playback range slider

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 /* get our elements */
 const videoPlayer = document.querySelector(".player");
 const video = videoPlayer.querySelector(".viewer");
-
+const progress = videoPlayer.querySelector(".progress");
 const progressBar = videoPlayer.querySelector(".progress_filled");
 const toggle = videoPlayer.querySelector(".toggle");
 const skipButtons = videoPlayer.querySelectorAll("[data-skip]");
-const ranges = videoPlayer.querySelectorAll(".player_sldier");
+const ranges = videoPlayer.querySelectorAll(".player_slider");
+let mousedown = false;
 /**Build out functions */
 function togglePlay() {
   const method = video.paused ? "play" : "pause";
@@ -18,13 +19,41 @@ function updateButton() {
 }
 
 function skip() {
-  console.log("skip");
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function sliderChange() {
+  video[this.name] = this.value;
+}
+function handleProgress() {
+  const videoPercent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${videoPercent}%`;
+}
+
+function progressBarSlider(e) {
+  const progressTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = progressTime;
 }
 
 /** Hook up event listeners */
 
+//video play and pause
 video.addEventListener("click", togglePlay);
+toggle.addEventListener("click", togglePlay);
+//play button icon change
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
-toggle.addEventListener("click", togglePlay);
-console.log(skipButtons);
+//skip button video currentTime increase decrease
+skipButtons.forEach((btn) => btn.addEventListener("click", skip));
+//range slider
+ranges.forEach((range) => range.addEventListener("change", sliderChange));
+//progress bar
+video.addEventListener("timeupdate", handleProgress);
+
+progress.addEventListener("click", progressBarSlider);
+progress.addEventListener(
+  "mousemove",
+  (e) => mousedown && progressBarSlider(e)
+);
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => (mousedown = false));
